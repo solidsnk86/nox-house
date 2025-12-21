a5178c92490bf5db1e0170b58ebae15c0e569e61
solidsnk86 <calcagni.gabriel86@gmail.com>
2025-12-21 04:00:37 -0300

    remove import

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { Header } from "@/components/header";
-import { Footer } from "@/components/footer";
 import { Hero, Menu, About, SphereView, Contact } from "@/components/sections";
 
 export default function Home() {
